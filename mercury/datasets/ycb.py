8e23e63d3fd955b3f50498d3ace2e56724a37cb3
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-03-17 23:26:14 +0900

    Download YCB_Video_Models from Gdrive

--- mercury/datasets/ycb.py
+++ mercury/datasets/ycb.py
@@ -1,9 +1,22 @@
+import gdown
 import path
 
 
+home = path.Path("~").expanduser()
+
+
 def get_visual_file(class_id):
+    root_dir = home / "data/ycb_video/YCB_Video_Models"
+
+    if not root_dir.exists():
+        gdown.cached_download(
+            url="https://drive.google.com/uc?id=1PKEJ8SVIVLukvmeIBexp6_XmJetHoOf2",  # NOQA
+            path=root_dir + ".zip",
+            md5="540c37435e4a16546850a83690b2db9b",
+            postprocess=gdown.extractall,
+        )
+
     assert class_id > 0
-    root_dir = path.Path("/home/wkentaro/data/ycb_video/YCB_Video_Models")
     class_names = []
     for model_dir in sorted(root_dir.listdir()):
         class_name = str(model_dir.basename())
