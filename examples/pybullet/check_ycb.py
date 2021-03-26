99967fb4a779e3acfbae43aa1e86f21b2552a024
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-03-26 19:11:44 +0900

    Update check_ycb.py selected objects

--- examples/pybullet/check_ycb.py
+++ examples/pybullet/check_ycb.py
@@ -24,7 +24,7 @@ def main():
 
     if args.type == "selected":
         # selected
-        class_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 16]
+        class_ids = [1, 2, 3, 4, 5, 7, 8, 9, 11, 12, 13, 16]
     elif args.type == "all":
         # all
         class_ids = list(range(1, 22))
