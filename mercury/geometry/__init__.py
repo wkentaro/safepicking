9d770ea46d3eb0ffd6b9bcb582f89e8404f74264
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-03-31 13:32:16 +0900

    Add geometry.normals_from_pointcloud

--- mercury/geometry/__init__.py
+++ mercury/geometry/__init__.py
@@ -4,6 +4,8 @@ from .coordinate import Coordinate
 
 from .look_at import look_at
 
+from .normals_from_pointcloud import normals_from_pointcloud
+
 from .opengl import *
 
 from .pointcloud_from_depth import pointcloud_from_depth
