0abff5244bec7e4c56be62c2d2b287459f1c5a6c
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-04-06 16:56:52 +0900

    Add masses and class_names to ycb.py

--- mercury/datasets/ycb.py
+++ mercury/datasets/ycb.py
@@ -1,4 +1,5 @@
 import gdown
+import numpy as np
 import path
 
 
@@ -36,3 +37,60 @@ def get_pcd_file(class_id):
     root_dir, class_names = init()
     class_name = class_names[class_id - 1]
     return root_dir / class_name / "points.xyz"
+
+
+class_names = [
+    "__background__",
+    "002_master_chef_can",
+    "003_cracker_box",
+    "004_sugar_box",
+    "005_tomato_soup_can",
+    "006_mustard_bottle",
+    "007_tuna_fish_can",
+    "008_pudding_box",
+    "009_gelatin_box",
+    "010_potted_meat_can",
+    "011_banana",
+    "019_pitcher_base",
+    "021_bleach_cleanser",
+    "024_bowl",
+    "025_mug",
+    "035_power_drill",
+    "036_wood_block",
+    "037_scissors",
+    "040_large_marker",
+    "051_large_clamp",
+    "052_extra_large_clamp",
+    "061_foam_brick",
+]
+class_names = np.array(class_names)
+class_names.setflags(write=0)
+
+
+# https://www.ri.cmu.edu/pub_files/2015/8/ycb_journal_v26.pdf
+masses = [
+    0,
+    0.414,
+    0.411,
+    0.514,
+    0.349,
+    0.603,
+    0.171,
+    0.187,
+    0.097,
+    0.370,
+    0.066,
+    0.178 + 0.066,
+    1.131,
+    0.147,
+    0.118,
+    0.895,
+    0.729,
+    0.082,
+    0.0158,
+    0.125,
+    0.202,
+    0.028,
+]
+masses = np.array(masses)
+masses.setflags(write=0)
