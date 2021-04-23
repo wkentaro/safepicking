5aa5ca6f4d3ca065bb2faef5d5bcebe13bdbdd9e
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-04-23 10:22:52 +0900

    Add min_threshold to average_distance_auc

--- mercury/geometry/average_distance_auc.py
+++ mercury/geometry/average_distance_auc.py
@@ -2,13 +2,17 @@ import numpy as np
 import sklearn.neighbors
 
 
-def average_distance_auc(reference, query, max_threshold=0.01, plot=False):
+def average_distance_auc(
+    reference, query, min_threshold=0, max_threshold=0.01, plot=False
+):
     kdtree = sklearn.neighbors.KDTree(reference)
     distances, _ = kdtree.query(query, k=1)
 
-    x = np.linspace(0, max_threshold)
+    x = np.linspace(min_threshold, max_threshold)
     y = [(distances <= xi).sum() / distances.size for xi in x]
-    auc = sklearn.metrics.auc(x, y) / max_threshold
+    auc = sklearn.metrics.auc(x - min_threshold, y) / (
+        max_threshold - min_threshold
+    )
 
     if plot:
         import matplotlib.pyplot as plt
