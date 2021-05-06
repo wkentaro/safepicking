8e53715633ad08f6de7e4d94b242e504cfbaeada
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-05-06 14:58:37 +0900

    Improve eval.py

--- examples/grasp_with_intent/eval.py
+++ examples/grasp_with_intent/eval.py
@@ -17,20 +17,15 @@ def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    # parser.add_argument("export_file", type=path.Path, help="export file")
     parser.add_argument(
         "--weight-dir", type=path.Path, help="weight dir", required=True
     )
-    # parser.add_argument("--seed", type=int, default=0, help="random seed")
     parser.add_argument("--nogui", action="store_true", help="no gui")
     args = parser.parse_args()
 
-    env = GraspWithIntentEnv(gui=not args.nogui)
+    env = GraspWithIntentEnv(gui=not args.nogui, retime=10)
     env.eval = True
-    obs = env.reset(
-        # random_state=np.random.RandomState(args.seed),
-        # pile_file=args.export_file,
-    )
+    obs = env.reset()
 
     log_dir = args.weight_dir.parent.parent
     with open(log_dir / "hparams.json") as f:
@@ -46,16 +41,18 @@ def main():
         act_result = agent.act(
             step=-1, observation=obs, deterministic=True, env=env
         )
-        imgviz.io.cv_imshow(agent.draw_act_summary(), "act_summary")
-        imgviz.io.cv_waitkey(100)
+
+        if 0:
+            imgviz.io.cv_imshow(agent.draw_act_summary(), "act_summary")
+            imgviz.io.cv_waitkey(100)
 
         transition = env.step(act_result)
         print(
             f"action={act_result.action}, reward={transition.reward}, "
             f"terminal={transition.terminal}",
         )
-        if transition.terminal:
-            break
+        if transition.info["needs_reset"]:
+            obs = env.reset()
         else:
             obs = transition.observation
 
