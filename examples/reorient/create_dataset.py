b888c4cd6d93a6061da44489a60c47418aaa75f2
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-05-28 13:14:48 +0900

    Save auc even without plan in create_dataset.py

--- examples/reorient/create_dataset.py
+++ examples/reorient/create_dataset.py
@@ -47,32 +47,29 @@ def main():
             ),
             10,
         ):
-            if "js_place_length" in result:
-                with pp.WorldSaver():
-                    pp.set_pose(env.fg_object_id, result["c_reorient"].pose)
-                    for _ in range(int(1 / pp.get_time_step())):
-                        pp.step_simulation()
-                    pose_actual = pp.get_pose(env.fg_object_id)
+            with pp.WorldSaver():
+                pp.set_pose(env.fg_object_id, result["c_reorient"].pose)
+                for _ in range(int(1 / pp.get_time_step())):
+                    pp.step_simulation()
+                pose_actual = pp.get_pose(env.fg_object_id)
 
-                pcd = np.loadtxt(
-                    mercury.datasets.ycb.get_pcd_file(
-                        class_id=common_utils.get_class_id(env.fg_object_id)
-                    )
+            pcd = np.loadtxt(
+                mercury.datasets.ycb.get_pcd_file(
+                    class_id=common_utils.get_class_id(env.fg_object_id)
                 )
-                reference = mercury.geometry.transform_points(
-                    pcd,
-                    mercury.geometry.transformation_matrix(
-                        *result["c_reorient"].pose
-                    ),
-                )
-                query = mercury.geometry.transform_points(
-                    pcd, mercury.geometry.transformation_matrix(*pose_actual)
-                )
-                auc = mercury.geometry.average_distance_auc(
-                    reference, query, max_threshold=0.2
-                )
-            else:
-                auc = np.nan
+            )
+            reference = mercury.geometry.transform_points(
+                pcd,
+                mercury.geometry.transformation_matrix(
+                    *result["c_reorient"].pose
+                ),
+            )
+            query = mercury.geometry.transform_points(
+                pcd, mercury.geometry.transformation_matrix(*pose_actual)
+            )
+            auc = mercury.geometry.average_distance_auc(
+                reference, query, max_threshold=0.2
+            )
 
             object_fg_flags = []
             object_classes = []
@@ -86,30 +83,27 @@ def main():
                 npz_file = (
                     home / f"data/mercury/reorient/n_class_5/{i:08d}.npz"
                 )
-
                 if not npz_file.exists():
-                    npz_file.parent.makedirs_p()
-                    np.savez_compressed(
-                        npz_file,
-                        **dict(
-                            object_fg_flags=object_fg_flags,
-                            object_classes=object_classes,
-                            object_poses=object_poses,
-                            grasp_pose=np.hstack(result["c_grasp"].pose),
-                            initial_pose=np.hstack(result["c_init"].pose),
-                            reorient_pose=np.hstack(result["c_reorient"].pose),
-                            js_place_length=result.get(
-                                "js_place_length", np.nan
-                            ),
-                            auc=auc,
-                        ),
-                    )
-                    logger.info(f"Saved to: {npz_file}")
                     break
-
                 i += process_num
                 if i >= args.size:
                     return
+
+            npz_file.parent.makedirs_p()
+            np.savez_compressed(
+                npz_file,
+                **dict(
+                    object_fg_flags=object_fg_flags,
+                    object_classes=object_classes,
+                    object_poses=object_poses,
+                    grasp_pose=np.hstack(result["c_grasp"].pose),
+                    initial_pose=np.hstack(result["c_init"].pose),
+                    reorient_pose=np.hstack(result["c_reorient"].pose),
+                    js_place_length=result.get("js_place_length", np.nan),
+                    auc=auc,
+                ),
+            )
+            logger.info(f"Saved to: {npz_file}")
 
 
 if __name__ == "__main__":
