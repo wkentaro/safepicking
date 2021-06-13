072713eb5c34f29b8f1199397e9792537fdcded6
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-06-13 23:42:42 +0900

    Use coase-to-fine features

--- examples/target_pick/conv_net.py
+++ examples/target_pick/conv_net.py
@@ -15,17 +15,29 @@ class ConvNet(torch.nn.Module):
             torch.nn.Conv2d(1, 4, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
         )
-        self.encoder = torch.nn.Sequential(
+
+        self.conv1 = torch.nn.Sequential(
             torch.nn.Conv2d(4 + 4, 4, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
+        )
+        self.conv2 = torch.nn.Sequential(
             torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
             torch.nn.Conv2d(4, 8, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
+        )
+        self.conv3 = torch.nn.Sequential(
             torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
             torch.nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
+        )
+        self.conv4 = torch.nn.Sequential(
             torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
             torch.nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+        )
+        self.conv5 = torch.nn.Sequential(
+            torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            torch.nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             torch.nn.ReLU(),
         )
 
@@ -33,7 +45,9 @@ class ConvNet(torch.nn.Module):
         # actions: 6
         # ee_poses: (episode_length - 1) * 7
         self.mlp = torch.nn.Sequential(
-            torch.nn.Linear(32 + 6 + (episode_length - 1) * 7, 32),
+            torch.nn.Linear(
+                4 + 8 + 16 + 32 + 64 + 6 + (episode_length - 1) * 7, 32
+            ),
             torch.nn.ReLU(),
             torch.nn.Linear(32, 32),
             torch.nn.ReLU(),
@@ -50,13 +64,24 @@ class ConvNet(torch.nn.Module):
         h_maskmap = self.encoder_maskmap(maskmap[:, None, :, :].float())
         h = torch.cat([h_heightmap, h_maskmap], dim=1)
 
-        h = self.encoder(h)
+        h_conv1 = self.conv1(h)
+        h_conv2 = self.conv2(h_conv1)
+        h_conv3 = self.conv3(h_conv2)
+        h_conv4 = self.conv4(h_conv3)
+        h_conv5 = self.conv5(h_conv4)
 
-        grasped_uv = torch.floor(grasped_uv.float() / 8).long()
-        u = grasped_uv[:, 0]
-        v = grasped_uv[:, 1]
+        uv = grasped_uv.long()
+        h_conv1 = h_conv1[torch.arange(B), :, uv[:, 1], uv[:, 0]]
+        uv = torch.floor(uv.float() / 2).long()
+        h_conv2 = h_conv2[torch.arange(B), :, uv[:, 1], uv[:, 0]]
+        uv = torch.floor(uv.float() / 2).long()
+        h_conv3 = h_conv3[torch.arange(B), :, uv[:, 1], uv[:, 0]]
+        uv = torch.floor(uv.float() / 2).long()
+        h_conv4 = h_conv4[torch.arange(B), :, uv[:, 1], uv[:, 0]]
+        uv = torch.floor(uv.float() / 2).long()
+        h_conv5 = h_conv5[torch.arange(B), :, uv[:, 1], uv[:, 0]]
 
-        h = h[torch.arange(B), :, v, u]
+        h = torch.cat([h_conv1, h_conv2, h_conv3, h_conv4, h_conv5], dim=1)
 
         h = h[:, None, :].repeat_interleave(A, dim=1)
         h_actions = actions[None, :, :].repeat_interleave(B, dim=0)
