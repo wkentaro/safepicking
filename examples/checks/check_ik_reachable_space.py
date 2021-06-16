13b0efdb954acd2c138e5680f325e0cd8e66f477
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-06-16 14:27:27 +0900

    Update check_ik_reachable_space.py

--- examples/checks/check_ik_reachable_space.py
+++ examples/checks/check_ik_reachable_space.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import time
 
 import imgviz
@@ -8,24 +9,46 @@ import pybullet_planning as pp
 
 import mercury
 
-import utils
-
 
 def main():
-    utils.init_world()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--type",
+        choices=["topdown", "backfront", "leftright", "rightleft"],
+        help="type",
+        required=True,
+    )
+    args = parser.parse_args()
+
+    pp.connect()
+    pp.add_data_path()
+
+    pp.load_pybullet("plane.urdf")
+    pp.enable_gravity()
 
     ri = mercury.pybullet.PandaRobotInterface()
     homej_quaternion = ri.get_pose("tipLink")[1]
 
-    eulers = [
-        [0, 0, 0],
-        [np.deg2rad(30), 0, 0],
-        [np.deg2rad(-30), 0, 0],
-        [0, np.deg2rad(30), 0],
-        [0, np.deg2rad(-30), 0],
-    ]
+    if args.type == "topdown":
+        eulers = [[0, 0, 0]]
+        sphere = pp.create_sphere(0.8, color=(1, 0, 0, 0.2), collision=False)
+        pp.set_pose(sphere, ([0, 0, 0.1], [0, 0, 0, 1]))
+    elif args.type == "backfront":
+        eulers = [[np.deg2rad(90), 0, 0]]
+        sphere = pp.create_sphere(0.8, color=(1, 0, 0, 0.2), collision=False)
+        pp.set_pose(sphere, ([0.3, 0, 0.3], [0, 0, 0, 1]))
+    elif args.type == "leftright":
+        eulers = [[0, np.deg2rad(90), 0]]
+        sphere = pp.create_sphere(0.8, color=(1, 0, 0, 0.2), collision=False)
+        pp.set_pose(sphere, ([0, -0.3, 0.3], [0, 0, 0, 1]))
+    elif args.type == "rightleft":
+        eulers = [[0, np.deg2rad(-90), 0]]
+        sphere = pp.create_sphere(0.8, color=(1, 0, 0, 0.2), collision=False)
+        pp.set_pose(sphere, ([0, 0.3, 0.3], [0, 0, 0, 1]))
 
-    if 0:
+    if 1:
         c_homej = mercury.geometry.Coordinate(*ri.get_pose("tipLink"))
         for euler in eulers:
             c = c_homej.copy()
@@ -34,13 +57,13 @@ def main():
             ri.setj(j)
             time.sleep(1)
 
-    aabb = ((-0.8, -0.8, 0), (0.8, 0.8, 0.5))
+    aabb = ((-0.8, -0.8, 0), (0.8, 0.8, 1.2))
     pp.draw_aabb(aabb)
 
     x, y, z = np.meshgrid(
-        np.linspace(aabb[0][0], aabb[1][0], num=10),
-        np.linspace(aabb[0][1], aabb[1][1], num=10),
-        np.linspace(aabb[0][2], aabb[1][2], num=5),
+        np.linspace(aabb[0][0], aabb[1][0], num=9),
+        np.linspace(aabb[0][1], aabb[1][1], num=9),
+        np.linspace(aabb[0][2], aabb[1][2], num=7),
     )
     xyz = np.stack([x, y, z], axis=3)
 
@@ -54,7 +77,7 @@ def main():
         for euler in eulers:
             c = c_cano.copy()
             c.rotate(euler)
-            j = ri.solve_ik(c.pose, rotation_axis="z")
+            j = ri.solve_ik(c.pose, n_init=1, rotation_axis="z")
             if j is not None:
                 num_solutions += 1
         num_solutions /= len(eulers)
@@ -68,7 +91,7 @@ def main():
             / 255
         )
 
-        box = pp.create_box(0.1, 0.1, 0.1, color=color.tolist() + [0.8])
+        box = pp.create_box(0.05, 0.05, 0.05, color=color.tolist() + [0.8])
         pp.set_pose(box, (point, (0, 0, 0, 1)))
 
     while True:
