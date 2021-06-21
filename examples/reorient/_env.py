6eb72947614ef04718cf8643e351975eb9d1851f
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-06-21 16:34:53 +0900

    Add js_home in planning

--- examples/reorient/_env.py
+++ examples/reorient/_env.py
@@ -372,8 +372,7 @@ class Env:
             pp.Attachment(self.ri.robot, self.ri.ee, obj_to_ee, object_id)
         ]
 
-        j = self.ri.homej
-        self.ri.setj(j)
+        self.ri.setj(self.ri.homej)
         self.ri.attachments[0].assign()
 
         with self.ri.enabling_attachments():
@@ -414,6 +413,26 @@ class Env:
             return False, result
 
         assert self.ri.attachments[0].child == self.fg_object_id
+
+        self.ri.setj(result["j_grasp"])
+        self.ri.attachments[0].assign()
+
+        js = self.ri.planj(
+            self.ri.homej,
+            obstacles=obstacles,
+            min_distances_start_goal={
+                (self.ri.attachments[0].child, -1): -0.01
+            },
+        )
+        if js is None:
+            logger.error(f"Failed to solve homing path: {act_result.action}")
+            before_return()
+            return False, result
+        result["js_home"] = js
+
+        self.ri.setj(js[-1])
+        self.ri.attachments[0].assign()
+
         js = self.ri.planj(
             result["j_pre_place"],
             obstacles=obstacles,
@@ -495,7 +514,8 @@ class Env:
             pp.Attachment(self.ri.robot, self.ri.ee, obj_to_ee, object_id)
         ]
 
-        for _ in (_ for _ in self.ri.movej(self.ri.homej)):
+        js = validation_result["js_home"]
+        for _ in (_ for j in js for _ in self.ri.movej(j)):
             pp.step_simulation()
             time.sleep(pp.get_time_step())
 
