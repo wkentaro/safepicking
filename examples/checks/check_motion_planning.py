d91a1811c3589aa6540303c2a91524d56c1d2678
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-08-08 14:27:07 +0100

    Fix check_motion_planning.py

--- examples/checks/check_motion_planning.py
+++ examples/checks/check_motion_planning.py
@@ -3,14 +3,14 @@
 import time
 
 import pybullet as p
-import pybullet_planning
+import pybullet_planning as pp
 
 import mercury
 
 
 def main():
-    pybullet_planning.connect(use_gui=True)
-    pybullet_planning.add_data_path()
+    pp.connect(use_gui=True)
+    pp.add_data_path()
 
     p.resetDebugVisualizerCamera(
         cameraDistance=2,
@@ -24,15 +24,16 @@ def main():
 
     # -------------------------------------------------------------------------
 
-    box = pybullet_planning.create_box(0.7, 0.1, 0.4)
+    box = pp.create_box(0.7, 0.1, 0.4)
     p.resetBasePositionAndOrientation(box, [0.5, 0, 0.2], [0, 0, 0, 1])
 
     # -------------------------------------------------------------------------
 
-    cube = pybullet_planning.create_box(
-        0.05, 0.05, 0.05, mass=0.1, color=(0, 1, 0, 1)
-    )
-    p.resetBasePositionAndOrientation(cube, (0, 0, 0.7), (0, 0, 0, 1))
+    cube = pp.create_box(0.05, 0.05, 0.05, mass=0.1, color=(0, 1, 0, 1))
+    ee_to_world = ri.get_pose("tipLink")
+    obj_to_ee = ([0, 0, 0.025], [0, 0, 0, 1])
+    obj_to_world = pp.multiply(ee_to_world, obj_to_ee)
+    p.resetBasePositionAndOrientation(cube, *obj_to_world)
     p.createConstraint(
         parentBodyUniqueId=ri.robot,
         parentLinkIndex=ri.ee,
@@ -40,21 +41,15 @@ def main():
         childLinkIndex=-1,
         jointType=p.JOINT_FIXED,
         jointAxis=(0, 0, 0),
-        parentFramePosition=(0, 0, 0.04),
-        parentFrameOrientation=(0, 0, 0, 1),
+        parentFramePosition=obj_to_ee[0],
+        parentFrameOrientation=obj_to_ee[1],
         childFramePosition=(0, 0, 0),
     )
-    ri.attachments = [
-        pybullet_planning.Attachment(
-            ri.robot, ri.ee, [(0, 0, 0.04), (0, 0, 0, 1)], cube
-        )
-    ]
+    ri.attachments = [pp.Attachment(ri.robot, ri.ee, obj_to_ee, cube)]
 
     # -------------------------------------------------------------------------
 
-    c1 = mercury.geometry.Coordinate(
-        *pybullet_planning.get_link_pose(ri.robot, ri.ee)
-    )
+    c1 = mercury.geometry.Coordinate(*pp.get_link_pose(ri.robot, ri.ee))
     c1.translate([0.4, -0.4, -0.4], wrt="world")
 
     c2 = c1.copy()
@@ -66,19 +61,23 @@ def main():
     j2 = ri.solve_ik(c2.pose)
 
     while True:
-        traj = ri.planj(j1, obstacles=obstacles)
+        traj = None
+        while traj is None:
+            traj = ri.planj(j1, obstacles=obstacles)
         for j in traj:
             for _ in ri.movej(j):
                 p.stepSimulation()
                 time.sleep(1 / 240)
 
-        traj = ri.planj(j2, obstacles=obstacles)
+        traj = None
+        while traj is None:
+            traj = ri.planj(j2, obstacles=obstacles)
         for j in traj:
             for _ in ri.movej(j):
                 p.stepSimulation()
                 time.sleep(1 / 240)
 
-    pybullet_planning.disconnect()
+    pp.disconnect()
 
 
 if __name__ == "__main__":
