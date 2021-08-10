2001d98a0211dd1b815e0170e0b0a4afd9cc1fb6
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-08-10 19:51:44 +0100

    panda_reorient -> panda_drl

--- mercury/_pybullet/panda_robot_interface.py
+++ mercury/_pybullet/panda_robot_interface.py
@@ -30,7 +30,7 @@ class PandaRobotInterface:
     ):
         self.pose = pose
 
-        urdf_file = here / "robots/franka_panda/panda_reorient.urdf"
+        urdf_file = here / "robots/franka_panda/panda_drl.urdf"
         self.robot_model = skrobot.models.urdf.RobotModelFromURDF(
             urdf_file=urdf_file
         )
