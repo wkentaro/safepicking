525b7dbf96234140357b97c29c03802afc93d1d7
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-08-16 10:25:41 +0100

    Fix plan_reorient to plan j_grasp and j_place

--- examples/reorient/_reorient.py
+++ examples/reorient/_reorient.py
@@ -161,6 +161,8 @@ def plan_reorient(env, grasp_pose, reorient_pose):
 
     # find self-collision-free j_grasp
     for dg in np.linspace(-np.pi, np.pi, num=6, endpoint=False):
+        env.ri.attachments = []
+
         c = mercury.geometry.Coordinate(*ee_af_to_world)
         c.rotate([0, 0, dg])
         j = env.ri.solve_ik(c.pose)
@@ -182,7 +184,9 @@ def plan_reorient(env, grasp_pose, reorient_pose):
                 move_target=env.ri.robot_model.attachment_link0,
                 thre=0.01,
                 rthre=np.deg2rad(10),
+                n_init=10,
             )
+        env.ri.attachments = []  # skip env.ri.attachments from validatej
         if j is not None and env.ri.validatej(j, obstacles=bg_object_ids):
             result["j_place"] = j
             break
