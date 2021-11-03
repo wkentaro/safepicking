309e764be10098702b6462837eabdae59d14be86
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-11-03 12:52:26 +0000

    Add conv_net model to SafepickingTaskInterface

--- examples/picking/download_pretrained_models.py
+++ examples/picking/download_pretrained_models.py
@@ -11,3 +11,9 @@ gdown.cached_download(
     path=here / "logs/20210709_005731-fusion_net-noise/weights/84500/q.pth",
     md5="886b36a99c5a44b54c513ec7fee4ae0d",
 )
+
+gdown.cached_download(
+    id="1mcI34DQunVDbc5F4ENhkk1-MRspQEleH",
+    path=here / "logs/20210706_194543-conv_net/weights/91500/q.pth",
+    md5="ebf2e7b874f322fe7f38d0e39375d943",
+)
--- ros/src/mercury_ros/nodes/safepicking_task_interface.py
+++ ros/src/mercury_ros/nodes/safepicking_task_interface.py
@@ -32,20 +32,34 @@ class SafepickingTaskInterface:
         self.base = base
 
         self._picking_env = _env.PickFromPileEnv()
-        self._agent = _agent.DqnAgent(
-            env=self._picking_env, model="fusion_net"
-        )
-        self._agent.build(training=False)
 
         example_dir = path.Path(mercury.__file__).parent / "examples/picking"
-        weight_dir = (
-            example_dir / "logs/20210709_005731-fusion_net-noise/weights/84500"
-        )
-        gdown.cached_download(
-            id="1MBfMHpfOrcMuBFHbKvHiw6SA5f7q1T6l",
-            path=weight_dir / "q.pth",
-            md5="886b36a99c5a44b54c513ec7fee4ae0d",
-        )
+        if 0:
+            # 0.465
+            model = "fusion_net"
+            weight_dir = (
+                example_dir
+                / "logs/20210709_005731-fusion_net-noise/weights/84500"
+            )
+            gdown.cached_download(
+                id="1MBfMHpfOrcMuBFHbKvHiw6SA5f7q1T6l",
+                path=weight_dir / "q.pth",
+                md5="886b36a99c5a44b54c513ec7fee4ae0d",
+            )
+        else:
+            # 0.507
+            model = "conv_net"
+            weight_dir = (
+                example_dir / "logs/20210706_194543-conv_net/weights/91500"
+            )
+            gdown.cached_download(
+                id="1mcI34DQunVDbc5F4ENhkk1-MRspQEleH",
+                path=weight_dir / "q.pth",
+                md5="ebf2e7b874f322fe7f38d0e39375d943",
+            )
+
+        self._agent = _agent.DqnAgent(env=self._picking_env, model=model)
+        self._agent.build(training=False)
         self._agent.load_weights(weight_dir)
 
         self._target_class_id = None
