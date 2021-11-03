c28f694613323f8da030de15c8b5e3fae4db09e6
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-11-03 11:48:08 +0000

    SafepickingTaskInterface with a single view

--- ros/src/mercury_ros/nodes/safepicking_task_interface.py
+++ ros/src/mercury_ros/nodes/safepicking_task_interface.py
@@ -14,6 +14,7 @@ import mercury
 from mercury.examples.picking import _agent
 from mercury.examples.picking import _env
 from mercury.examples.picking import _get_heightmap
+from mercury.examples.picking import _utils
 
 import cv_bridge
 from morefusion_ros.msg import ObjectClassArray
@@ -47,63 +48,23 @@ class SafepickingTaskInterface:
         )
         self._agent.load_weights(weight_dir)
 
-        self._subscriber_safepicking = MessageSubscriber(
+        self._target_class_id = None
+        self._sub_singleview = MessageSubscriber(
             [
-                ("/camera/octomap_server/output/class", ObjectClassArray),
                 ("/camera/aligned_depth_to_color/image_raw", Image),
-                ("/camera/octomap_server/output/label_rendered", Image),
+                (
+                    "/camera/mask_rcnn_instance_segmentation/output/class",
+                    ObjectClassArray,
+                ),
+                (
+                    "/camera/mask_rcnn_instance_segmentation/output/label_ins",
+                    Image,
+                ),
+                ("/singleview_3d_pose_estimation/output", ObjectPoseArray),
             ]
         )
 
-    def rosmsgs_to_obs(self):
-        cam_msg = rospy.wait_for_message(
-            "/camera/color/camera_info", CameraInfo
-        )
-        cls_msg, depth_msg, label_msg = self._subscriber_safepicking.msgs
-
-        K = np.array(cam_msg.K).reshape(3, 3)
-
-        bridge = cv_bridge.CvBridge()
-        depth = bridge.imgmsg_to_cv2(depth_msg)
-        assert depth.dtype == np.uint16
-        depth = depth.astype(np.float32) / 1000
-        depth[depth == 0] = np.nan
-
-        # -2: unknown, -1: background, 0: instance_0, 1: instance_1, ...
-        label = bridge.imgmsg_to_cv2(label_msg).copy()
-        label[label == -2] = -1
-
-        class_id_to_instance_ids = collections.defaultdict(list)
-        for cls in cls_msg.classes:
-            class_id_to_instance_ids[cls.class_id].append(cls.instance_id)
-        class_id_to_instance_ids = dict(class_id_to_instance_ids)
-
-        camera_to_base = self.base.lookup_transform(
-            "panda_link0",
-            cam_msg.header.frame_id,
-            time=cam_msg.header.stamp,
-            timeout=rospy.Duration(1),
-        )
-
-        pcd_in_camera = mercury.geometry.pointcloud_from_depth(
-            depth, fx=K[0, 0], fy=K[1, 1], cx=K[0, 2], cy=K[1, 2]
-        )
-        pcd_in_base = mercury.geometry.transform_points(
-            pcd_in_camera,
-            mercury.geometry.transformation_matrix(*camera_to_base),
-        )
-
-        self.obs = dict(
-            camera_to_base=camera_to_base,
-            K=K,
-            depth=depth,
-            label=label,
-            class_id_to_instance_ids=class_id_to_instance_ids,
-            pcd_in_camera=pcd_in_camera,
-            pcd_in_base=pcd_in_base,
-        )
-
-    def get_grasp_poses(self, target_class_id):
+    def _get_grasp_poses(self):
         camera_to_base = self.obs["camera_to_base"]
         depth = self.obs["depth"]
         label = self.obs["label"]
@@ -114,7 +75,9 @@ class SafepickingTaskInterface:
             pcd_in_camera
         )
 
-        instance_id = self.obs["class_id_to_instance_ids"][target_class_id][0]
+        instance_id = self.obs["class_id_to_instance_ids"][
+            self._target_class_id
+        ][0]
         mask = ~np.isnan(depth) & (label == instance_id)
         pcd_in_camera = pcd_in_camera[mask]
         pcd_in_base = pcd_in_base[mask]
@@ -134,34 +97,123 @@ class SafepickingTaskInterface:
         grasp_poses = np.hstack((pcd_in_base, quaternion_in_base))
         return grasp_poses
 
-    def look_at_pile(self, *args, **kwargs):
-        self.look_at(eye=[0.5, 0, 0.7], target=[0.5, 0, 0], *args, **kwargs)
+    def _look_at_pile(self, *args, **kwargs):
+        self.base.look_at(
+            eye=[0.5, 0, 0.7], target=[0.5, 0, 0], *args, **kwargs
+        )
 
-    def run(self, place=True):
-        TARGET_CLASS_ID = 3
+    def capture_pile(self):
+        if self.base._env.object_ids:
+            for obj_id in self.base._env.object_ids:
+                pp.remove_body(obj_id)
+        self.base._env.object_ids = None
+        self.base._env.fg_object_id = None
 
-        self.base.init_workspace()
+        self._look_at_pile()
 
-        self.look_at_pile()
-
-        self._subscriber_safepicking.subscribe()
+        self._sub_singleview.subscribe()
         self.base.start_passthrough()
         rospy.sleep(5)
         while True:
-            if not self._subscriber_safepicking.msgs:
+            if not self._sub_singleview.msgs:
                 continue
-            cls_msg = self._subscriber_safepicking.msgs[0]
-            if TARGET_CLASS_ID not in [
-                cls.class_id for cls in cls_msg.classes
-            ]:
+            class_ids_detected = [
+                c.class_id for c in self._sub_singleview.msgs[1].classes
+            ]
+            if self._target_class_id not in class_ids_detected:
                 continue
             break
         self.base.stop_passthrough()
-        self._subscriber_safepicking.unsubscribe()
+        self._sub_singleview.unsubscribe()
 
-        self.rosmsgs_to_obs()
+        camera_msg = rospy.wait_for_message(
+            "/camera/color/camera_info", CameraInfo
+        )
+        (
+            depth_msg,
+            cls_msg,
+            label_msg,
+            obj_poses_msg,
+        ) = self._sub_singleview.msgs
 
-        grasp_poses = self.get_grasp_poses(target_class_id=TARGET_CLASS_ID)
+        K = np.array(camera_msg.K).reshape(3, 3)
+
+        bridge = cv_bridge.CvBridge()
+        depth = bridge.imgmsg_to_cv2(depth_msg)
+        assert depth.dtype == np.uint16
+        depth = depth.astype(np.float32) / 1000
+        depth[depth == 0] = np.nan
+
+        # -2: unknown, -1: background, 0: instance_0, 1: instance_1, ...
+        label = bridge.imgmsg_to_cv2(label_msg).copy()
+        label[label == -2] = -1
+
+        class_id_to_instance_ids = collections.defaultdict(list)
+        for cls in cls_msg.classes:
+            class_id_to_instance_ids[cls.class_id].append(cls.instance_id)
+        class_id_to_instance_ids = dict(class_id_to_instance_ids)
+
+        target_instance_id = class_id_to_instance_ids[self._target_class_id][0]
+
+        camera_to_base = self.base.lookup_transform(
+            "panda_link0",
+            camera_msg.header.frame_id,
+            time=camera_msg.header.stamp,
+            timeout=rospy.Duration(1),
+        )
+
+        pcd_in_camera = mercury.geometry.pointcloud_from_depth(
+            depth, fx=K[0, 0], fy=K[1, 1], cx=K[0, 2], cy=K[1, 2]
+        )
+        pcd_in_base = mercury.geometry.transform_points(
+            pcd_in_camera,
+            mercury.geometry.transformation_matrix(*camera_to_base),
+        )
+
+        assert self.base._env.object_ids is None
+        self.base._env.object_ids = []
+        for i, obj_pose_msg in enumerate(obj_poses_msg.poses):
+            pose = obj_pose_msg.pose
+            instance_id = obj_pose_msg.instance_id
+            class_id = obj_pose_msg.class_id
+            position = (pose.position.x, pose.position.y, pose.position.z)
+            quaternion = (
+                pose.orientation.x,
+                pose.orientation.y,
+                pose.orientation.z,
+                pose.orientation.w,
+            )
+            obj_to_camera = (position, quaternion)
+            obj_to_base = pp.multiply(camera_to_base, obj_to_camera)
+            visual_file = mercury.datasets.ycb.get_visual_file(class_id)
+            obj_id = mercury.pybullet.create_mesh_body(
+                visual_file=visual_file,
+                position=obj_to_base[0],
+                quaternion=obj_to_base[1],
+            )
+            if instance_id == target_instance_id:
+                self.base._env.fg_object_id = obj_id
+            self.base._env.object_ids.append(obj_id)
+
+        self.obs = dict(
+            camera_to_base=camera_to_base,
+            K=K,
+            depth=depth,
+            label=label,
+            class_id_to_instance_ids=class_id_to_instance_ids,
+            pcd_in_camera=pcd_in_camera,
+            pcd_in_base=pcd_in_base,
+            target_instance_id=target_instance_id,
+        )
+
+    def run(self, place=True):
+        self.base.init_workspace()
+
+        self._target_class_id = 3
+
+        self.capture_pile()
+
+        grasp_poses = self._get_grasp_poses()
 
         centroid = np.mean(grasp_poses[:, :3], axis=0)
         dist_from_centroid = np.linalg.norm(
@@ -195,7 +247,6 @@ class SafepickingTaskInterface:
 
         js_extract = self.plan_extraction(
             j_init=j,
-            target_class_id=TARGET_CLASS_ID,
             grasp_pose=grasp_pose,
         )
 
@@ -225,7 +276,7 @@ class SafepickingTaskInterface:
             js_grasp.append(j)
         js_grasp = js_grasp[::-1]
 
-        js_place = self.plan_placement(j_init=self.base.pi.homej)
+        js_place = self._plan_placement(j_init=self.base.pi.homej)
 
         self.base.movejs(js_grasp, time_scale=10)
 
@@ -251,7 +302,7 @@ class SafepickingTaskInterface:
 
             self.base.reset_pose()
 
-    def plan_placement(self, j_init):
+    def _plan_placement(self, j_init):
         bin_to_base = pp.get_pose(self._bin)
 
         with pp.WorldSaver():
@@ -278,11 +329,12 @@ class SafepickingTaskInterface:
 
         return js_place
 
-    def plan_extraction(self, j_init, target_class_id, grasp_pose):
+    def _get_heightmap(self, center_xy):
+        center = np.array([center_xy[0], center_xy[1], np.nan])
         aabb = np.array(
             [
-                grasp_pose[0] - self._picking_env.HEIGHTMAP_SIZE / 2,
-                grasp_pose[0] + self._picking_env.HEIGHTMAP_SIZE / 2,
+                center - self._picking_env.HEIGHTMAP_SIZE / 2,
+                center + self._picking_env.HEIGHTMAP_SIZE / 2,
             ]
         )
         aabb[0][2] = self.base._env.TABLE_OFFSET - 0.05
@@ -295,43 +347,24 @@ class SafepickingTaskInterface:
             pixel_size=self._picking_env.HEIGHTMAP_PIXEL_SIZE,
         )
         idmap -= 1  # 0: background -> -1: background
-        target_instance_id = self.obs["class_id_to_instance_ids"][
-            target_class_id
-        ][0]
+        return heightmap, idmap
+
+    def _plan_extraction(self, j_init, grasp_pose):
+        heightmap, idmap = self._get_heightmap(center_xy=grasp_pose[0][:2])
+        target_instance_id = self.obs["target_instance_id"]
         maskmap = idmap == target_instance_id
 
-        obj_poses_msg = rospy.wait_for_message(
-            "/object_mapping/output/poses", ObjectPoseArray
-        )
-        assert obj_poses_msg.header.frame_id == "panda_link0"
-
-        num_instance = len(obj_poses_msg.poses)
+        num_instance = len(self.base._env.object_ids)
         grasp_flags = np.zeros((num_instance,), dtype=np.uint8)
         object_labels = np.zeros(
             (num_instance, len(self._picking_env.CLASS_IDS)), dtype=np.int8
         )
         object_poses = np.zeros((num_instance, 7), dtype=np.float32)
-        assert self.base._env.object_ids is None
-        self.base._env.object_ids = []
-        for i, obj_pose_msg in enumerate(obj_poses_msg.poses):
-            pose = obj_pose_msg.pose
-            instance_id = obj_pose_msg.instance_id
-            class_id = obj_pose_msg.class_id
-            position = (pose.position.x, pose.position.y, pose.position.z)
-            quaternion = (
-                pose.orientation.x,
-                pose.orientation.y,
-                pose.orientation.z,
-                pose.orientation.w,
-            )
-            visual_file = mercury.datasets.ycb.get_visual_file(class_id)
-            obj_id = mercury.pybullet.create_mesh_body(
-                visual_file=visual_file,
-                position=position,
-                quaternion=quaternion,
-            )
-
-            grasp_flags[i] = instance_id == target_instance_id
+        for i in range(num_instance):
+            obj_id = self.base._env.object_ids[i]
+            class_id = _utils.get_class_id(obj_id)
+            position, quaternion = pp.get_pose(obj_id)
+            grasp_flags[i] = obj_id == self.base._env.fg_object_id
             object_label = self._picking_env.CLASS_IDS.index(class_id)
             object_labels[i] = np.eye(len(self._picking_env.CLASS_IDS))[
                 object_label
@@ -345,11 +378,6 @@ class SafepickingTaskInterface:
                 quaternion[2],
                 quaternion[3],
             ]
-
-            if grasp_flags[i]:
-                self.base._env.fg_object_id = obj_id
-            else:
-                self.base._env.object_ids.append(obj_id)
 
         ee_poses = np.zeros(
             (self._picking_env.episode_length, 7), dtype=np.float32
@@ -436,6 +464,34 @@ class SafepickingTaskInterface:
         world_saver.restore()
 
         return js
+
+    def test_heightmap_change(self):
+        self._target_class_id = 5
+
+        self.capture_pile()
+        heightmap, idmap = self._get_heightmap(
+            self.base._env.PILE_POSITION[:2]
+        )
+        heightmap[idmap == self.obs["target_instance_id"]] = np.nan
+        heightmap[heightmap == 0] = np.nan
+        heightmap1 = heightmap
+
+        input("Move an object and press key to continue:")
+
+        self.capture_pile()
+        heightmap, idmap = self._get_heightmap(
+            self.base._env.PILE_POSITION[:2]
+        )
+        heightmap[idmap == self.obs["target_instance_id"]] = np.nan
+        heightmap[heightmap == 0] = np.nan
+        heightmap2 = heightmap
+
+        diff = abs(heightmap1 - heightmap2)
+        mask = diff > 0.01
+        print(
+            diff[mask].sum(),
+            mask.sum() / mask.size,
+        )
 
 
 def main():
